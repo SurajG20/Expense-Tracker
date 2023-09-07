8daf153f3a9ba321f1302d3bcd374b1b5d6ae9c3
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-09-07 20:54:31 +0530

    Expense Model and Functionality

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const { addIncome, deleteIncome, getIncome } = require("../controllers/income");
+const {
+  addExpense,
+  deleteExpense,
+  getExpense,
+} = require("../controllers/expense");
 router
   .post("/add-income", addIncome)
   .delete("/delete-income/:id", deleteIncome)
-  .get("/get-incomes", getIncome);
+  .get("/get-incomes", getIncome)
+  .post("/add-expense", addExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .get("/get-expenses", getExpense);
 
 module.exports = router;
